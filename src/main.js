6a5252e51f4bcc76f8a0bf7ff2340316b03cc807
creature137 <xoxorich@proton.me>
2024-05-07 16:46:00 +0300

    add menu anim

---
+++ src/main.js
@@ -0,0 +1,28 @@
+(() => {
+  const mobileMenu = document.querySelector('.mob-menu');
+  const openMenuBtn = document.querySelector('.menu-btn');
+  const closeMenuBtn = document.querySelector('.menu-close-btn');
+  const closeMenuLink = document.querySelectorAll('.close-menu-link');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+  closeMenuLink.forEach(item => item.addEventListener('click', toggleMenu));
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
